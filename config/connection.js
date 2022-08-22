30cb78baa745ea5f51f17fd49eb238dd72d59ae1
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-08-22 15:16:01 -0500

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,22 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
 
-const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, 
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql', 
-    logging: false
-  }
-);
+let sequelize;
 
-module.exports = connection;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
+
